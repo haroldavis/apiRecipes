ee9a833d176a0e70098b6d3e70dba3f1e18ed859
haroldavis <haroldavis91@gmail.com>
2020-08-27 17:57:58 -0500

    i create file contans for test and i was try create query, mutation with users and tasks

---
+++ constans/index.js
@@ -0,0 +1,13 @@
+module.exports.tasks = [
+  {_id: "1", name: "Work", completed: false, userId: "3"},
+  {_id: "2", name: "Eat", completed: true, userId: "1"},
+  {_id: "3", name: "Shopping", completed: true, userId: "4"},
+  {_id: "4", name: "Gym", completed: true, userId: "2"}
+];
+
+module.exports.users = [
+  {_id: "1", name: "Kevin", email: 'kevin@gmail.com' },
+  {_id: "2", name: "Monica", email: 'monica@gmail.com'},
+  {_id: "3", name: "Jimena", email: 'jimena@gmail.com' },
+  {_id: "4", name: "Harold", email: 'harold@gmail.com' }
+];
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const { ApolloServer, gql } = require('apollo-server-express');
 const cors = require('cors');
 const dotEnv = require('dotenv');
 
+const { tasks, users } = require('./constans')
+const uuid = require('uuid')
+
 dotEnv.config()
 
 const app = express()
@@ -14,13 +17,65 @@ app.use(express.json())
 const typeDefs = gql`
   type Query {
     greetings: [String!]
-    name: String
+    tasks: [Task!]
+    task(id: ID!): Task
+    users: [User!]
+    user(id: ID!): User
+  }
+
+  input createTaskInput{
+    name: String!
+    completed: Boolean!
+    userId: ID!
+  }
+
+  type Mutation{
+    createTask(input: createTaskInput!): Task
+  }
+
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    task: [Task!]
+  }
+
+  type Task {
+    _id: ID!
+    name: String!
+    completed: Boolean!
+    user: User!
   }
 `
 const resolvers = {
   Query: {
-    greetings : () => ['hi', 'hello'],
-    name: () => 'harold'
+    greetings : () => null,
+    tasks: () => {
+      console.log(tasks);
+      return tasks
+    },
+    task: (_, {id}) => {
+      console.log(typeof id)
+      return tasks.find(task => task._id === id)
+    },
+    users: () => users,
+    user: (_, {id}) => users.find(user => user._id === id)
+  },
+  Mutation: {
+    createTask: (_, { input }) => {
+      const task = { ...input, _id: uuid.v4() };
+      tasks.push(task);
+      return task
+    }
+  },
+  Task: {
+    user: ({userId}) =>{
+      console.log('userId', userId)
+      return users.find(user => user._id === userId)
+    }
+  },
+  User: {
+    task: ({_id}) => tasks.filter(task => task._id === _id)
   }
 }
 
